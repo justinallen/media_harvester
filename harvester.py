f00fe3a6d8551512929617d735ff19df59ae5f62
justin allen <j@justinallen.us>
2016-07-20 17:09:51 -0700

    first commit - working video scraper / audio ripper

---
+++ harvester.py
@@ -0,0 +1,61 @@
+from __future__ import unicode_literals
+from sys import argv
+import os
+import time
+import youtube_dl
+import subprocess
+from slugify import slugify
+
+script, url = argv
+
+print "harvesting..."
+print "from url:", url
+
+options = {
+    'format': 'mp4',
+    'outtmpl': '%(title)s.%(ext)s'
+    # 'postprocessors': [{
+    #     'key': 'FFmpegExtractAudio',
+    #     'preferredcodec': 'mp3',
+    #     'preferredquality': '192',
+    # }],	
+}
+
+with youtube_dl.YoutubeDL(options) as ydl:
+	info = ydl.extract_info(url, download=False)
+	# grab the video title
+	video_title = info.get('title', None)
+	# slugify it for the 
+	dir_name = slugify(video_title)
+	# make directory
+	print('creating ' + dir_name + ' directory')
+	os.mkdir(dir_name)
+	# go to that directory
+	os.chdir(dir_name)	
+	# print('my title is ' + info.get('title', None))
+	ydl.download([url])
+
+# create file for meta info
+print('creating metadata file')
+meta = open("about.txt", 'w')
+# write meta info to that file
+date = time.strftime('%m.%d.%Y')
+meta.write('Harvested on ' + date + '\n')
+meta.write('Title: ' + video_title + '\n')
+meta.write('From: ' + url + '\n')
+meta.close()
+
+# now for the audio
+print('now for the audio rip...')
+
+# prep audio filename
+filename = video_title + '.mp4'
+audio_filename = video_title + '.mp3'
+
+# create mp3 version
+# subprocess is like issuing a unix command
+# ffmpeg must be installed
+subprocess.call(['ffmpeg', '-i', filename, audio_filename])
+
+
+
